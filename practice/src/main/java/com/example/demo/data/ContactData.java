b9296fb911895dce10ff7b519766266e72c5295c
KAZUKI-TANAKA-0116 <t.tanaka.cingroup2012gmail.com>
2024-10-24 12:39:01 +0900

    お問い合わせフォーム実装　＃２

---
+++ practice/src/main/java/com/example/demo/data/ContactData.java
@@ -0,0 +1,13 @@
+package com.example.demo.data;
+
+public class ContactData {
+	private String lastName;
+	private String firstName;
+	private String email;
+	private String phone;
+	private String zipcode;
+	private String address;
+	private String buildingName;
+	private String contactType;
+	private String body;
+}
